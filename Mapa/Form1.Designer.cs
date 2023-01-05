aa4f249c56b7fbccfe69523e59b23b46d3e45f80
Luna Hazuki <LunaHazuki2006@outlook.com>
2023-01-05 19:34:15 -0400

    vista hecha

--- Mapa/Form1.Designer.cs
+++ Mapa/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Mapa
 {
-    partial class Form1
+    partial class FormMapa
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -28,13 +28,164 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.gMapControl = new GMap.NET.WindowsForms.GMapControl();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.labelLatitud = new System.Windows.Forms.Label();
+            this.labelLongitud = new System.Windows.Forms.Label();
+            this.buttonCargar = new System.Windows.Forms.Button();
+            this.textBoxLatitud = new System.Windows.Forms.TextBox();
+            this.textBoxLongitud = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 75.44118F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 24.55882F));
+            this.tableLayoutPanel1.Controls.Add(this.gMapControl, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 82.7044F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 17.2956F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(713, 319);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // gMapControl
+            // 
+            this.gMapControl.Bearing = 0F;
+            this.gMapControl.CanDragMap = true;
+            this.gMapControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gMapControl.EmptyTileColor = System.Drawing.Color.Navy;
+            this.gMapControl.GrayScaleMode = false;
+            this.gMapControl.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.gMapControl.LevelsKeepInMemory = 5;
+            this.gMapControl.Location = new System.Drawing.Point(6, 5);
+            this.gMapControl.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.gMapControl.MarkersEnabled = true;
+            this.gMapControl.MaxZoom = 2;
+            this.gMapControl.MinZoom = 2;
+            this.gMapControl.MouseWheelZoomEnabled = true;
+            this.gMapControl.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.gMapControl.Name = "gMapControl";
+            this.gMapControl.NegativeMode = false;
+            this.gMapControl.PolygonsEnabled = true;
+            this.gMapControl.RetryLoadTile = 0;
+            this.gMapControl.RoutesEnabled = true;
+            this.gMapControl.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.gMapControl.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.gMapControl.ShowTileGridLines = false;
+            this.gMapControl.Size = new System.Drawing.Size(525, 253);
+            this.gMapControl.TabIndex = 0;
+            this.gMapControl.Zoom = 0D;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.labelLatitud, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.labelLongitud, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.buttonCargar, 0, 4);
+            this.tableLayoutPanel2.Controls.Add(this.textBoxLatitud, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.textBoxLongitud, 0, 3);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(543, 5);
+            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 5;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(164, 253);
+            this.tableLayoutPanel2.TabIndex = 1;
+            // 
+            // labelLatitud
+            // 
+            this.labelLatitud.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.labelLatitud.AutoSize = true;
+            this.labelLatitud.Location = new System.Drawing.Point(3, 13);
+            this.labelLatitud.Name = "labelLatitud";
+            this.labelLatitud.Size = new System.Drawing.Size(78, 23);
+            this.labelLatitud.TabIndex = 0;
+            this.labelLatitud.Text = "Latitud:";
+            // 
+            // labelLongitud
+            // 
+            this.labelLongitud.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.labelLongitud.AutoSize = true;
+            this.labelLongitud.Location = new System.Drawing.Point(3, 113);
+            this.labelLongitud.Name = "labelLongitud";
+            this.labelLongitud.Size = new System.Drawing.Size(94, 23);
+            this.labelLongitud.TabIndex = 1;
+            this.labelLongitud.Text = "Longitud:";
+            // 
+            // buttonCargar
+            // 
+            this.buttonCargar.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.buttonCargar.BackColor = System.Drawing.Color.Lavender;
+            this.buttonCargar.Location = new System.Drawing.Point(16, 207);
+            this.buttonCargar.Name = "buttonCargar";
+            this.buttonCargar.Size = new System.Drawing.Size(131, 38);
+            this.buttonCargar.TabIndex = 2;
+            this.buttonCargar.Text = "Cargar";
+            this.buttonCargar.UseVisualStyleBackColor = false;
+            this.buttonCargar.Click += new System.EventHandler(this.buttonCargar_Click);
+            // 
+            // textBoxLatitud
+            // 
+            this.textBoxLatitud.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxLatitud.BackColor = System.Drawing.Color.SeaShell;
+            this.textBoxLatitud.Location = new System.Drawing.Point(3, 59);
+            this.textBoxLatitud.Name = "textBoxLatitud";
+            this.textBoxLatitud.Size = new System.Drawing.Size(158, 31);
+            this.textBoxLatitud.TabIndex = 3;
+            // 
+            // textBoxLongitud
+            // 
+            this.textBoxLongitud.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxLongitud.BackColor = System.Drawing.Color.SeaShell;
+            this.textBoxLongitud.Location = new System.Drawing.Point(3, 159);
+            this.textBoxLongitud.Name = "textBoxLongitud";
+            this.textBoxLongitud.Size = new System.Drawing.Size(158, 31);
+            this.textBoxLongitud.TabIndex = 4;
+            // 
+            // FormMapa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 23F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.LavenderBlush;
+            this.ClientSize = new System.Drawing.Size(713, 319);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Font = new System.Drawing.Font("Book Antiqua", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.Name = "FormMapa";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mapa";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private GMap.NET.WindowsForms.GMapControl gMapControl;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Label labelLatitud;
+        private System.Windows.Forms.Label labelLongitud;
+        private System.Windows.Forms.Button buttonCargar;
+        private System.Windows.Forms.TextBox textBoxLatitud;
+        private System.Windows.Forms.TextBox textBoxLongitud;
     }
 }
 
--- Mapa/Form1.cs
+++ Mapa/Form1.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace Mapa
 {
-    public partial class Form1 : Form
+    public partial class FormMapa : Form
     {
-        public Form1()
+        public FormMapa()
         {
             InitializeComponent();
+        }
+
+        private void buttonCargar_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
